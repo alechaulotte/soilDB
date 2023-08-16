2204af689918e613c9c07bf64c82dfc861c74b89
Alec Haulotte <alec@alechaulotte.com>
2023-08-16 09:06:58 -0700

    Update get_vegplot_data_from_NASIS_db.R
    canopy cover

--- R/get_vegplot_data_from_NASIS_db.R
+++ R/get_vegplot_data_from_NASIS_db.R
@@ -281,7 +281,7 @@ get_vegplot_transpecies_from_NASIS_db <-  function(SS = TRUE,
   # veg transect species data - many species to one veg transect
   q.vtps <- "SELECT siteiid, siteobsiid, vegplotiid, vegtransectiidref as vegtransect_id, vegplotid, vegplotname,
     obsdate, vegtransplantsummiid as vtpsiid, vtps.seqnum, plantsym, plantsciname,
-    plantnatvernm, plantnativity, planttypegroup, 
+    speciescancovclass, plantnatvernm, plantnativity, planttypegroup, 
     plantheightcllowerlimit, plantheightclupperlimit, sociabilityclass, 
     specieslivecanhtbotave, specieslivecanhttopave, overstorydbhmin, 
     overstorydbhmax, speciesovercancovpct, speciesovercancovclass, 
